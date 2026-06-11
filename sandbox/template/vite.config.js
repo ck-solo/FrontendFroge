6399cabb843c1c626d0ae1a69dda58bf7d500c0c
Chandan Kumar <solouser019@gmail.com>
2026-06-12 04:51:36 +0530

    feat: initialize vite configuration with react plugin and custom dev server settings

--- sandbox/template/vite.config.js
+++ sandbox/template/vite.config.js
@@ -4,18 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json']
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
     allowedHosts: true,
     hmr: {
       clientPort: 80
+    },
+    watch: {
+      usePolling: true,
+      interval: 300,
+      ignored: [ "node_modules" ]
     }
-  },
-
-  watch: {
-    usePolling: true,
-    interval: 300,
-    ignored: [ "node_modules" ]
   }
 })
